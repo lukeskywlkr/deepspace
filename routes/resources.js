f5cf738a6567f0f8dd9990d64ef84be8ea3d9044
Luca De Carne <luca.decarne@edu.unito.it>
2016-11-10 12:17:31 +0100

    Changed the resources route file in order to make it work with the logic.

--- routes/resources.js
+++ routes/resources.js
@@ -1,9 +1,23 @@
 const logic = require('../logic/resources.js');
 module.exports = [
-  { method: 'POST', path: '/resources', handler: function(request, reply){
-    return reply('Ok');
-  }},
-  { method: 'GET', path: '/resources', handler: function(request, reply) {
-    return reply(logic.greetings());
-  }}
+    {
+        method: 'POST',
+        path: '/resources',
+        config: {
+          payload: {
+            maxBytes: 209715200,
+            output: 'stream',
+            parse: true
+          },
+          handler: function(request, reply) {
+            return logic.upload(request, reply);
+          }
+        }
+    }, {
+        method: 'GET',
+        path: '/resources',
+        handler: function(request, reply) {
+            return logic.greetings(request, reply);
+        }
+    }
 ];
